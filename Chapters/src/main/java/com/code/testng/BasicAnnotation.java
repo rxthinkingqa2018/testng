e26a757a0239047536a2231cb6eff1ed04763093
rxthingqa <wanghui@rxthinking.com>
2019-01-22 11:44:04 +0800

    all data

---
+++ Chapters/src/main/java/com/code/testng/BasicAnnotation.java
@@ -0,0 +1,11 @@
+package com.code.testng;
+
+import org.testng.annotations.Test;
+
+public class BasicAnnotation {
+
+    @Test
+    public void testcase1(){
+        System.out.println("测试用例1");
+    }
+}
